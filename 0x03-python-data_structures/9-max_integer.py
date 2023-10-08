d52680dd037cf215200f4a9b779201eb8e9e25b8
rahwalegesse <rahwalegesse31@gmail.com>
2023-10-08 08:06:57 -0700

    task9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return (None)
+
+    lar = my_list[0]
+    for j in range(len(my_list)):
+        if my_list[j] > lar:
+            lar = my_list[j]
+
+    return (lar)
