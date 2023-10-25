116b8ee02d0f54da5dfe03f59850de9e9b917eb0
rahwalegesse <rahwalegesse31@gmail.com>
2023-10-25 07:17:59 -0700

    task1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square class."""
+
+
+class Square:
+    """Defines a class function."""
+
+    def __init__(self, size):
+        """Constructor.
+        Args:
+            size: size square.
+        """
+        self.__size = size
