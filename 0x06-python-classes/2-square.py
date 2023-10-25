b979f7b6d2b8f62d58ff62286d90d1fc72d141da
rahwalegesse <rahwalegesse31@gmail.com>
2023-10-25 07:21:52 -0700

    task2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Define function."""
+
+
+class Square:
+    """Represent class."""
+
+    def __init__(self, size=0):
+        """Initialize a class function.
+        Args:
+            size (int): size.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
