922467a6a14c4d03025cddb7de4d851b02182fe2
rahwalegesse <rahwalegesse31@gmail.com>
2023-11-02 14:43:52 -0700

    tdd

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,27 +1,17 @@
 #!/usr/bin/python3
-"""Module for add_integer method."""
+
+"""Defines an integer addition function."""
 
 
 def add_integer(a, b=98):
-    """Adds two numbers.
-
-    Args:
-        a: first number.
-        b: second number.
-
+    """Return the integer addition of a and b.
+    Float arguments are typecasted to ints before addition is performed.
     Raises:
-        TypeError: if a and b are not either int or float.
-
-    Returns:
-        The sum of two numbers.
+        TypeError: If either of a or b is a non-integer and non-float.
     """
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
 
-    if type(a) not in (int, float):
-        raise TypeError('a must be an integer')
-    if type(b) not in (int, float):
-        raise TypeError('b must be an integer')
-    return int(a) + int(b)
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/0-add_integer.txt")
