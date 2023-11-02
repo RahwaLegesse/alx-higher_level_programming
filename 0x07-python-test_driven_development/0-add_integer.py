08f388d95110332071e6729e0db2fd779e604c0c
rahwalegesse <rahwalegesse31@gmail.com>
2023-11-02 14:15:10 -0700

    Test-driven development

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module for add_integer method."""
+
+
+def add_integer(a, b=98):
+    """Adds two numbers.
+
+    Args:
+        a: first number.
+        b: second number.
+
+    Raises:
+        TypeError: if a and b are not either int or float.
+
+    Returns:
+        The sum of two numbers.
+    """
+
+    if type(a) not in (int, float):
+        raise TypeError('a must be an integer')
+    if type(b) not in (int, float):
+        raise TypeError('b must be an integer')
+    return int(a) + int(b)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
