b4afcb8bcdd9ba873213a8d751198601f0461439
rahwalegesse <rahwalegesse31@gmail.com>
2024-01-26 09:30:55 -0800

    task0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-"""Fetche the url"""
-import urllib.request
+"""Fetches the URL"""
+
+from urllib.request import Request, urlopen
 
 
 if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        byte = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(byte)))
-        print("\t- content: {}".format(byte))
-        print("\t- utf8 content: {}".format(byte.decode("utf-8")))
+    ask = Request('https://intranet.hbtn.io/status')
+
+    with urlopen(ask) as answer:
+        content = answer.read()
+        utf8_content = content.decode('utf-8')
+
+        print('Body response:')
+        print('\t- type: {_type}'.format(_type=type(content)))
+        print('\t- content: {_content}'.format(_content=content))
+        print('\t- utf8 content: {_utf8_c}'.format(_utf8_c=utf8_content))
