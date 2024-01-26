0c170d6f7e1884506cbb32132641cf5646f0ef6f
rahwalegesse <rahwalegesse31@gmail.com>
2024-01-26 09:43:40 -0800

    task3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL, sends a request to the URL
-and displays.
-"""
-from urllib import request, error
+"""Takes in a URL, sends a request to the URL and"""
+
 from sys import argv
+from urllib.request import Request, urlopen
+from urllib.parse import urlencode
+from urllib.error import HTTPError
+
 
 if __name__ == "__main__":
-    url = sys.argv[1]
+    ask = Request(argv[1])
 
-    request = urllib.request.Request(url)
     try:
-        with urllib.request.urlopen(request) as response:
-            print(response.read().decode("ascii"))
-    except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code))
+        with urlopen(ask) as response:
+            print(response.read().decode('utf-8'))
+    except HTTPError as ex:
+        print('Error code:', ex.code)
