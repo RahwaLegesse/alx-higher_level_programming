7fb64804df88d4f8fc56883144808523bfe70ce5
rahwalegesse <rahwalegesse31@gmail.com>
2024-02-20 05:36:11 -0800

    number6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,10 +5,10 @@ request(process.argv[2], function (err, response, body) {
     const todos = JSON.parse(body);
     const completed = {};
     todos.forEach((todo) => {
-      if (todo.completed && completed[todo.user] === undefined) {
-        completed[todo.user] = 1;
+      if (todo.completed && completed[todo.userid] === undefined) {
+        completed[todo.userid] = 1;
       } else if (todo.completed) {
-        completed[todo.user] += 1;
+        completed[todo.userid] += 1;
       }
     });
     console.log(completed);
