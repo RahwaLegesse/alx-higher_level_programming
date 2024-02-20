c572c17f3fc50be874d506c5bbcd664fbd247af1
rahwalegesse <rahwalegesse31@gmail.com>
2024-02-20 04:44:08 -0800

    number4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,11 +2,11 @@
 const request = require('request');
 request(process.argv[2], function (err, response, body) {
   if (!err) {
-    const resu = JSON.parse(body).resu;
-    console.log(resu.reduce((c, movie) => {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
       return movie.characters.find((character) => character.endsWith('/18/'))
-        ? c + 1
-        : c;
+        ? count + 1
+        : count;
     }, 0));
   }
 });
